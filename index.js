fd7095e985b3378b71b8cc025d463ead56bccd4a
JacksonBates <malgalin@gmail.com>
2016-07-08 20:59:55 +1000

    adds file size parsing and response

--- index.js
+++ index.js
@@ -10,10 +10,14 @@ app.set( 'views', __dirname + '/views' );
 app.set( 'view engine', 'ejs' );
 
 app.get( '/' , function ( req, res ) {
-  res.render( 'index' );
+  res.render( 'pages/index' );
 });
 
-
+app.post( '/', upload.single( 'uploadFile' ), function ( req, res ) {
+  var response = req.file;
+  console.log( response );
+  res.end(JSON.stringify( { size: response.size }));
+});
 
 app.listen( app.get( 'port' ), function() {
   console.log( 'Node app is running on port ', app.get( 'port' ));
